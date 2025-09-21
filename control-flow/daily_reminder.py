a6fe7e8b952a0731263f1a9c5e5dc7f3e1d707eb
Winnie Banice <93024882+wacerabanice@users.noreply.github.com>
2025-09-21 10:34:37 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,22 +4,33 @@ time_bound: input('Is it time-bound?(yes/no):')
 
 Match priority:
 case 'high': 
-    message = 'task, is a high priority task,'
-    label = 'Reminder'
-case 'medium':
-    message = 'task, is a medium priority task.'
-    label = 'Reminder'
-case 'low':
-    message = 'task, is a low priority task.'
-    label = 'Note'
+    message = 'task, is a high priority task'
+    if time_bound == 'yes':
+        message += 'that requires immediate attention today!'
+    else:
+    message += 'Consider completing it when you have free time.'
+    print('Reminder:', message)
+
+case "medium":
+    message = 'task, is a medium priority task'
+    if time_bound == 'yes':
+        message += 'that requires immediate attention today!'
+    else:
+    message += 'Consider completing it when you have free time.'
+    print('Reminder:', message)
+
+case "low":
+    message = 'task, is a low priority task'
+    if time_bound == 'yes':
+        message += 'that requires immediate attention today!'
+    else:
+    message += 'Consider completing it when you have free time.'
+    print('Note:', message)
+
 case _:
     message = 'task, has an unspecified priority'
-
-if time_bound == 'yes':
-    message += 'that requires immediate attention today!'
-else:
+    if time_bound == 'yes':
+        message += 'that requires immediate attention today!'
+    else:
     message += 'Consider completing it when you have free time.'
-
-
-print(label:, message)
-
+    print('Note:', message)
